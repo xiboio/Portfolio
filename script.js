880d6196337af274cce6d5d21d523333855ab534
xiboio <murilodaniel02@gmail.com>
2024-11-11 15:55:16 -0300

    fix js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 document.addEventListener("DOMContentLoaded"), () => {
     const loadingScreen = document.getElementById("loading-screen");
-    const portfolioContent = document.getElementById("portfolio-content");
+
 }
 
 setTimeout(() => {
     loadingScreen.style.display = "none";
-    portfolioContent.style.display = "block";
 }, 3000);
